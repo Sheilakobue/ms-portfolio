1fc774d5c213d273879a3b9f5dcd9b886d321ba4
Sheila Kobue <Maserole.mojela@gmail.com>
2024-07-05 19:20:29 +0200

    updated client file

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,6 +1,7 @@
 import { createClient } from 'next-sanity'
 
 import { apiVersion, dataset, projectId, useCdn } from '../env'
+import imageUrlBuilder from "@sanity/image-url"
 
 export const client = createClient({
   projectId,
@@ -9,3 +10,8 @@ export const client = createClient({
   useCdn,
   perspective: 'published',
 })
+const builder = imageUrlBuilder(client)
+
+export function urlFor(source:string){
+  return builder.image(source)
+}
