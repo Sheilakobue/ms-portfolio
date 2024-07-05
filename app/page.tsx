0674d4966043cb8d082a4e387e50767d00e18de0
Sheila Kobue <Maserole.mojela@gmail.com>
2024-07-05 16:25:50 +0200

    added a hero page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-// import Hero from '@/components/Hero'; // Import the Hero component from the specified path
+import Hero from '@/components/Hero'; // Import the Hero component from the specified path
 
 /**
  * The Home function is a React functional component that renders the Hero component.
@@ -9,6 +9,6 @@
 export default function Home() {
   // Return the Hero component wrapped in a fragment
   return (
-    // <Hero/>
+    <Hero/>
   );
 }
