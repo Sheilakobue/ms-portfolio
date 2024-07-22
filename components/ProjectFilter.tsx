2dfbc017d7d3769f6868e4eeb641ce28e4963cf2
Sheila Kobue <Maserole.mojela@gmail.com>
2024-07-22 14:54:12 +0200

    Update ProjectFilter.tsx

--- components/ProjectFilter.tsx
+++ components/ProjectFilter.tsx
@@ -1,7 +1,71 @@
-import React from 'react'
+"use client";
 
-export default function ProjectFilter() {
-  return (
-    <div>ProjectFilter</div>
-  )
+import { useEffect } from "react";
+
+interface Project {
+  name: string;
+  image: string;
+  url: string;
+  category?: string;
 }
+
+interface ProjectFilterProps {
+  setFiltered: React.Dispatch<
+    React.SetStateAction<Project[]>
+  >;
+  activeCategory: string;
+  setActiveCategory: React.Dispatch<
+    React.SetStateAction<string>
+  >;
+  projects: Project[];
+}
+
+const ProjectFilter: React.FC<ProjectFilterProps> = ({
+  setFiltered,
+  activeCategory,
+  setActiveCategory,
+  projects,
+}) => {
+  useEffect(() => {
+    if (activeCategory === "all") {
+      setFiltered(projects);
+      return;
+    }
+    const filtered = projects.filter((project) =>
+      project.category?.includes(activeCategory)
+    );
+    setFiltered(filtered);
+  }, [activeCategory]);
+
+  const categories = [
+    { key: "all", label: "All" },
+    { key: "next", label: "Next.js" },
+    { key: "react", label: "React" },
+    { key: "vanilla", label: "Vanilla" },
+    { key: "mongo", label: "MongoDB" },
+    { key: "tailwind", label: "Tailwind" },
+    { key: "prisma", label: "Prisma" },
+    { key: "sql", label: "SQL" },
+  ];
+  return (
+    <div className="flex gap-8 my-10 items-center flex-wrap justify-center">
+      {categories.map((category) => (
+        <button
+          key={category.key}
+          className={`text-blue-500 
+        ${
+          activeCategory === category.key
+            ? "border-yellow-500 border-b-2"
+            : ""
+        }
+        `}
+          onClick={() => setActiveCategory(category.key)}
+        >
+          {category.label}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default ProjectFilter;
