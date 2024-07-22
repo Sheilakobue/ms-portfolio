39f71d07ec3c522882aec96c6fc3198123cb2e59
Sheila Kobue <Maserole.mojela@gmail.com>
2024-07-22 14:53:47 +0200

    created a Portfolio page

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -13,39 +13,35 @@ const Portfolio: React.FC<{ projects: Project[] }>
 = ({ projects }) => {
   const [activeCategory, setActiveCategory] = 
   useState<string>("all");
-  const [filtered, setFiltered] = useState<Project[]>([]);
+  const [filtered, setFiltered] = 
+  useState<Project[]>([]);
   const [visibleProjects, setVisibleProjects] = useState<number>(2);
 
-  // useEffect(() => {
-  //   if (activeCategory === "all") {
-  //     setFiltered(projects);
-  //   } else {
-  //     setFiltered(projects.filter(project => project.category === activeCategory));
-  //   }
-  // }, [activeCategory, projects]);
-
   const loadMoreProjects = () => {
-    setVisibleProjects(prevProjects => prevProjects + 2);
+    setVisibleProjects(
+    (prevProjects) => prevProjects + 2
+  );
   };
 
   return (
-    <div id="projects" className="w-full h-fit py-10 relative bg-blue-300">
+    <div id="projects" className="w-full h-fit py-10 relative bg-blue-200">
       <div className="max-w-[1400px] mx-auto w-[91%]">
         <div className="flex justify-center">
-          <span className="bg-yellow-300 px-2 text-xl font-extrabold border uppercase rounded-md md-5">
+          <span className="bg-cyan-400 px-2 text-xl font-extrabold border uppercase rounded-md md-5">
             Projects
           </span>
         </div>
+
         <ProjectFilter 
           setFiltered={setFiltered}
           activeCategory={activeCategory}
           setActiveCategory={setActiveCategory}
           projects={projects}
         />
+
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-10">
-          {filtered
-          .slice(0, visibleProjects)
-          .map((project, index) => (
+          {filtered.slice(0, visibleProjects).map
+          ((project, index) => (
             <ProjectCard 
             key={index} 
             name = {project.name}
