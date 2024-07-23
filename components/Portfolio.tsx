8969e3a310d5d200e2faa4bf13a5fa9e3140f723
Sheila Kobue <Maserole.mojela@gmail.com>
2024-07-23 10:29:21 +0200

    commented the portfolio file

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -1,38 +1,61 @@
-"use client";
+"use client"; // Indicates that this code should be run on the client side
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ProjectFilter from "./ProjectFilter";
 import ProjectCard from "./ProjectCard";
 
+// Interface for the Project type
 interface Project {
   name: string;
   image: string;
   url: string;
+  category?: string;
 }
-const Portfolio: React.FC<{ projects: Project[] }> = ({
-  projects,
-}) => {
-  const [activeCategory, setActiveCategory] =
-    useState<string>("all");
-  const [filtered, setFiltered] = useState<Project[]>([]);
-  const [visibleProjects, setVisibleProjects] =
-    useState<number>(2);
 
+/**
+ * Portfolio is a React functional component that displays a portfolio of projects.
+ * It includes a filter for projects and a "Show More" button to load more projects.
+ *
+ * @component
+ * @param {Object} props - The props that the component receives.
+ * @param {Project[]} props.projects - The list of projects to display.
+ * @returns {JSX.Element} The rendered Portfolio component.
+ */
+const Portfolio: React.FC<{ projects: Project[] }> 
+= ({ projects}) => {
+  const [activeCategory, setActiveCategory] = useState<string>("all"); // State to manage the active category for filtering projects
+  const [filtered, setFiltered] = useState<Project[]>([]); // State to manage the filtered list of projects
+  const [visibleProjects, setVisibleProjects] = useState<number>(3); // State to manage the number of visible projects
+
+  // Load more projects by increasing the visibleProjects state by 4
   const loadMoreProjects = () => {
-    setVisibleProjects((prevProjects) => prevProjects + 2);
+    setVisibleProjects((prevProjects) => prevProjects + 3);
   };
+
+  // Effect to update the filtered list of projects whenever the active category changes
+  useEffect(() => {
+    if (activeCategory === "all") {
+      setFiltered(projects); // Show all projects if the active category is "all"
+      return;
+    }
+    const filteredProjects = projects.filter((project) =>
+      project.category?.includes(activeCategory)
+    );
+    setFiltered(filteredProjects); // Update the filtered list based on the active category
+  }, [activeCategory, projects]);
 
   return (
     <div
       id="projects"
-      className="w-full h-fit py-10 relative bg-white"
+      className="w-full h-fit py-10 relative bg-blue-100"
     >
       <div className="max-w-[1400px] mx-auto w-[91%]">
         <div className="flex justify-center">
-          <span className="bg-yellow-500 px-2 text-xl font-extrabold border uppercase rounded-md mb-5">
+          <span className="bg-yellow-300 px-2 text-xl text-blue-950 font-extrabold border uppercase rounded-md mb-5">
             Projects
           </span>
         </div>
+        {/* Project filter component to filter projects based on categories */}
         <ProjectFilter
           setFiltered={setFiltered}
           activeCategory={activeCategory}
@@ -55,9 +78,10 @@ const Portfolio: React.FC<{ projects: Project[] }> = ({
 
         {visibleProjects < filtered.length && (
           <div className="text-center mt-4">
-            <button className="bg-yellow-500 py-2 px-4 rounded-md font-bold mt-10"
-            onClick={loadMoreProjects}
-            aria-label="Show More Projects"
+            <button
+              className="bg-yellow-500 py-2 px-4 rounded-md font-bold mt-10"
+              onClick={loadMoreProjects}
+              aria-label="Show More Projects"
             >
               Show More
             </button>
--- components/ProjectFilter.tsx
+++ components/ProjectFilter.tsx
@@ -1,7 +1,8 @@
-"use client";
+"use client"; // Indicates that this code should be run on the client side
 
 import { useEffect } from "react";
 
+// Interface for the Project type
 interface Project {
   name: string;
   image: string;
@@ -9,34 +10,41 @@ interface Project {
   category?: string;
 }
 
+// Interface for the props that ProjectFilter component accepts
 interface ProjectFilterProps {
-  setFiltered: React.Dispatch<
-    React.SetStateAction<Project[]>
-  >;
+  setFiltered: React.Dispatch<React.SetStateAction<Project[]>>;
   activeCategory: string;
-  setActiveCategory: React.Dispatch<
-    React.SetStateAction<string>
-  >;
+  setActiveCategory: React.Dispatch<React.SetStateAction<string>>;
   projects: Project[];
 }
 
+/**
+ * ProjectFilter is a React functional component that filters projects
+ * based on the selected category and updates the filtered list.
+ *
+ * @component
+ * @param {ProjectFilterProps} props - The props that the component receives.
+ * @returns {JSX.Element} The rendered ProjectFilter component.
+ */
 const ProjectFilter: React.FC<ProjectFilterProps> = ({
   setFiltered,
   activeCategory,
   setActiveCategory,
   projects,
 }) => {
+  // useEffect hook to update the filtered projects list whenever the active category changes
   useEffect(() => {
     if (activeCategory === "all") {
-      setFiltered(projects);
+      setFiltered(projects); // Show all projects if the active category is "all"
       return;
     }
     const filtered = projects.filter((project) =>
       project.category?.includes(activeCategory)
     );
-    setFiltered(filtered);
-  }, [activeCategory]);
+    setFiltered(filtered); // Update the filtered list based on the active category
+  }, [activeCategory, projects, setFiltered]);
 
+  // List of categories for filtering projects
   const categories = [
     { key: "all", label: "All" },
     { key: "next", label: "Next.js" },
@@ -47,18 +55,15 @@ const ProjectFilter: React.FC<ProjectFilterProps> = ({
     { key: "prisma", label: "Prisma" },
     { key: "sql", label: "SQL" },
   ];
+
   return (
     <div className="flex gap-8 my-10 items-center flex-wrap justify-center">
       {categories.map((category) => (
         <button
           key={category.key}
-          className={`text-gray-500 
-        ${
-          activeCategory === category.key
-            ? "border-yellow-500 border-b-2"
-            : ""
-        }
-        `}
+          className={`text-gray-500 ${
+            activeCategory === category.key ? "border-yellow-500 border-b-2" : ""
+          }`}
           onClick={() => setActiveCategory(category.key)}
         >
           {category.label}
