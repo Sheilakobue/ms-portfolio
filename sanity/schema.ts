0532b7929e79d05d0dc73b6285ea1be655e9c293
Sheila Kobue <Maserole.mojela@gmail.com>
2024-07-24 09:40:40 +0200

    documented schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,6 +1,10 @@
-import { type SchemaTypeDefinition } from 'sanity';
+// Importing necessary type definitions from 'sanity'
+import { SchemaTypeDefinition } from 'sanity';
+
+// Importing the project schema definition from './project'
 import project from './project';
 
+// Defining the schema object with types property as an array of SchemaTypeDefinition
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [project],
+  types: [project], // Adding the project schema to the array of schema types
 }
