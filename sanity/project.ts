767221b99738a6287e38b78717d27d21e8eb7e50
Sheila Kobue <Maserole.mojela@gmail.com>
2024-07-24 09:38:53 +0200

    updated file

--- sanity/project.ts
+++ sanity/project.ts
@@ -1,24 +1,35 @@
 export default {
+    // Name of the schema
     name: 'project',
+    
+    // Title of the schema
     title: 'Project',
+    
+    // Type of the schema
     type: 'document',
+    
+    // Array of fields in the schema
     fields: [
         {
+            // Field for the name of the project
             name: 'name',
             title: 'Name',
             type: 'string',
         },
         {
+            // Field for the image of the project
             name: 'image',
             title: 'Image',
             type: 'image',
         },
         {
+            // Field for the URL of the project
             name: 'url',
             title: 'URL',
             type: 'url',
         },
         {
+            // Field for the category of the project
             name: 'category',
             title: 'Category',
             type: 'array',
