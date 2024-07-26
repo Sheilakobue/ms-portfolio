ed5638e8bb36c63435ac8f20b8d8204a5d326cf4
Sheila Kobue <Maserole.mojela@gmail.com>
2024-07-26 13:35:34 +0200

    added certificats

--- components/Background.tsx
+++ components/Background.tsx
@@ -4,9 +4,82 @@ import TabButton from "./TabButton";
 import { useState, useEffect, useTransition } from "react";
 import Link from "next/link";
 
+
+const TAB_DATA = [
+  {
+    title:"SoftSkills",
+    id:"softSkills",
+    content:(
+      <ul>
+        <li>Attention to detail</li>
+        <li>Problem solving</li>
+        <li>Creativity</li>
+        <li>Communication Skills</li>
+        <li>Team Player</li>
+        <li>Well Organized</li>
+      </ul>
+    )
+  },
+  {
+    title:"Education",
+    id:"education",
+    content:(
+      <ul>
+        <li>Sterkrevier Combined School</li>
+        <li>2005</li>
+        <li>Grade 12</li>
+        <li>English, Sepedi, Afrikaans, Mathematics, Physical Science, Biology, History.</li>
+      
+        <li>Central Johannesburg College (CJC)</li>
+        <li>2009</li>
+        <li>N1-N6</li>
+        <li>Civil Engineering</li>
+        <li>December 2022</li>
+        <li>Programming Fundamentals <a href="https://www.codespace.co.za/">Codespace</a></li>
+                    
+        <li>March 2023</li>
+        <li>Intro to Web <a href="https://www.codespace.co.za/">Codespace</a></li>
+      </ul>
+    ),
+  },
+  {
+    title:"Certificates",
+    id:"certificates",
+    content:(
+      <ul>
+        <li>Software Engineering</li>
+        <li>Next.js & React</li>
+        <li>Computer Literacy & Basic Computer</li>
+        <li>MicroSoft Certificate</li>
+        <li>Program Fundamental</li>
+        <li>Growthmindset</li>
+      </ul>
+    )
+  },
+  {
+    title:"Experience",
+    id:"experience",
+    content:(
+      <ul>
+       <li>Edcon Call Centre</li>
+       <li>01/04/2009 - 30/02/2011</li>
+       <li>Call Centre Agent</li>
+      <li>Customer services, collection, inbound and outbound new accounts, capturing customer's information on Devox</li>
+      <li>Maipi Electrical & Civil Construction</li>
+      <li>01/07/2012 - 12/04/2021</li>
+      <li>Office Administrator</li>
+      <li>Office work: paper works, phone calls, filing, attend meetings, attend workshops, research, welcome visitors, data capture, safety file updates, tender applications, finance: Invoicing, quotations, payroll admin.</li>
+      <li>Statistic South Africa</li>
+      <li>27/12/2021 - 30/07/2022</li>
+      <li>Field Operation Officer (FOO)</li>
+      <li>Coordinate and supervise the work assigned to Fieldworkers and Field supervisors, Control visit and refusals from field work supervisors</li>
+      </ul>
+    )
+  },
+]
 export default function Background() {
 
-  const [tab, setTab] = useState<string>("skills"); // State to manage the active tab
+  const [tab, setTab] = useState<string>("softSkills"); // State to manage the active tab
   const [isPending, startTransition] = useTransition(); // Manage state transitions
 
   // Function to handle tab changes
@@ -21,13 +94,18 @@ export default function Background() {
   };
 
   return (
-    <section 
-    id="background"
-    className="w-full h-fit px-[40px] py-20 relative bg-blue-100"
+    <div
   >
   
     <div className="flex justify-items-end items-left my-10  flex-wrap flex-row mt-8">
       
+        <TabButton
+          selectTab={() => handleTabChange("softSkills")}
+          active={tab === "softSkills"}
+        >
+          Soft SKills
+        </TabButton>
+
         <TabButton
           selectTab={() => handleTabChange("education")}
           active={tab === "education"}
@@ -35,8 +113,8 @@ export default function Background() {
           Education
         </TabButton>
         <TabButton
-          selectTab={() => handleTabChange("certifications")}
-          active={tab === "certifications"}
+          selectTab={() => handleTabChange("certificates")}
+          active={tab === "certificates"}
         >
           Certifications
         </TabButton>
@@ -47,7 +125,11 @@ export default function Background() {
           Experience
         </TabButton>        
       </div>
-  </section>
+      <div className='mt-8'>
+        {TAB_DATA.find((t) => t.id === tab).content}
+
+      </div>
+  </div>
 
   )
 }
