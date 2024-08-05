ed3f97d157a8fb5503a285e5e1c99c43baafd586
Sheila Kobue <Maserole.mojela@gmail.com>
2024-08-05 12:44:59 +0200

    imported files

--- components/ParticlesBackground.tsx
+++ components/ParticlesBackground.tsx
@@ -1,9 +1,15 @@
 "use client"; // This directive indicates that the code is client-side
 
-import { useEffect, useState } from "react"; // Importing useEffect and useState hooks from React
-import Particles, { initParticlesEngine } from "@tsparticles/react"; // Importing Particles and initParticlesEngine from @tsparticles/react
+import  {useEffect, useMemo, useState } from "react"; // Importing useEffect and useState hooks from React
+import Particles, {initParticlesEngine,} from "@tsparticles/react"; // Importing Particles and initParticlesEngine from @tsparticles/react
 import particlesConfig from "@/lib/particles-config"; // Importing custom particles configuration
 import { loadFull } from "tsparticles"; // Importing loadFull from tsparticles
+import {
+  type Container,
+  type ISourceOptions,
+  MoveDirection,
+  OutMode,
+} from "@tsparticles/engine";
 
 /**
  * ParticlesBackground component initializes and renders a particles background using tsparticles.
@@ -11,34 +17,38 @@ import { loadFull } from "tsparticles"; // Importing loadFull from tsparticles
  * @component
  * @returns {JSX.Element} The rendered ParticlesBackground component.
  */
+
 const ParticlesBackground = () => {
   const [init, setInit] = useState(false); // State to track if the particles engine has been initialized
 
   useEffect(() => {
     // Initialize the particles engine
     initParticlesEngine(async (engine) => {
-      await loadFull(engine); // Load the full tsparticles engine
+      await loadFull(engine);
     }).then(() => {
-      setInit(true); // Set the initialization state to true after loading
+      setInit(true);
     });
   }, []);
 
   // Function to handle particles loaded event
-  const particlesLoaded = async (container?: Container): Promise<void> => {
-    console.log(container); // Log the container once particles are loaded
+  const particlesLoaded = async (
+    container?: Container
+  ): Promise<void> => {
+    console.log(container);
   };
 
-  if (init) {
+if (init) {
     return (
       <Particles
-        id="tsparticles" // ID for the particles container
-        particlesLoaded={particlesLoaded} // Event handler for particles loaded
-        options={particlesConfig as ISourceOptions} // Custom particles configuration
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={particlesConfig as ISourceOptions}
       />
     );
   }
 
   return <></>; // Render nothing if not initialized yet
 };
+
 
 export default ParticlesBackground;
